aa44e89b7f4acf6451cbf74cb9058a9605f4cf7a
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2021-12-16 09:00:20 +0100

    Implement submit and refresh button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 import './style.css';
+
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+const nameInput = document.getElementById('name');
+const scoreInput = document.getElementById('score');
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', async () => {
+  await postData(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QG3sxOihEfABZTgB6FYu/scores/',
+    { user: nameInput.value, score: +scoreInput.value },
+  );
+  nameInput.value = '';
+  scoreInput.value = '';
+});
+
+const refreshButton = document.getElementById('refresh');
+const scoresUl = document.getElementsByClassName('scores')[0];
+
+const loadScores = async () => {
+  scoresUl.innerHTML = '';
+  const { result: scores } = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QG3sxOihEfABZTgB6FYu/scores/')
+    .then((response) => response.json());
+  scores.forEach((score) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${score.user}: ${score.score}`;
+    scoresUl.appendChild(li);
+  });
+};
+
+refreshButton.addEventListener('click', loadScores);
+window.onload = loadScores;
